7f2174d6fbb751feb800a2ff2b50c3e2f72c5543
abishek.g <abishek.g@mail.aquilogix.com>
2019-02-26 11:32:16 +0530

    Hooks- useState and useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import ResourceList from './ResourceList';
 
-export class App extends Component {
-  state = {
-    resource: 'posts'
-  }
-  render() {
-    return (
+const App = () => {
+  //Array Destructuring.
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState({ resource: 'posts' })} className="ui button primary">Posts</button>
-          <button onClick={() => this.setState({ resource: 'todos' })} className="ui button primary">Todos</button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource('posts')} className="ui button primary">Posts</button>
+        <button onClick={() => setResource('todos')} className="ui button primary">Todos</button>
       </div>
-    )
-  }
+      <ResourceList resource={resource} />
+    </div>
+  )
 }
 
 export default App
